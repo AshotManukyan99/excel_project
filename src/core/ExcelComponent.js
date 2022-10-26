76da8bf0308b48de8336782221ed6b0f9a0d615c
AshotManukyan99 <ashot.1999.manuk@gmail.com>
2022-10-26 16:18:37 +0400

    OOP Setup

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,9 @@
+import {DomListener} from "@core/DomListener";
+
+export class ExcelComponent extends DomListener {
+    //  returns the template of the components
+    toHTML() {
+        return ''
+    }
+
+}
