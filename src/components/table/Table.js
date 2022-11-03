3add64251a20c4aaacdfeed0fd134d7e786bd9e7
AshotManukyan99 <ashot.1999.manuk@gmail.com>
2022-11-03 18:00:11 +0400

    Create Table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,65 +1,12 @@
 import {ExcelComponent} from "@core/ExcelComponent";
+import {createTable} from "@/components/table/table.template";
 
-export class Table  extends ExcelComponent   {
+export class Table extends ExcelComponent {
     static className = 'excel__table'
 
     toHTML() {
-        return `
-                 <div class="row">
-        <div class="row-info">
-
-        </div>
-
-        <div class="row-data">
-          <div class="columns">
-            A
-          </div>
-          <div class="columns">
-            B
-          </div>
-          <div class="columns">
-            C
-          </div>
-        </div>
-
-      </div>
-       <div class="row">
-        <div class="row-info">
-          1
-        </div>
-
-        <div class="row-data">
-          <div class="cell  selected" contenteditable>
-            A1
-          </div>
-          <div class="cell" contenteditable>
-            B2
-          </div>
-          <div class="cell" contenteditable>
-            C3
-          </div>
-        </div>
-
-      </div>
-        <div class="row">
-        <div class="row-info">
-          2
-        </div>
-
-        <div class="row-data">
-          <div class="cell  selected" contenteditable>
-            A1
-          </div>
-          <div class="cell" contenteditable>
-            B2
-          </div>
-          <div class="cell" contenteditable>
-            C3
-          </div>
-        </div>
-
-      </div>
-        `
+        return createTable(100)
     }
 
 }
+
